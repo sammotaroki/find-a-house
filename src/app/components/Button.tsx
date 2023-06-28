2cac6d2fdc1f2908bf7dbafeedd7823853e833ec
Samm Motaroki <sammotaroki@gmail.com>
2023-06-28 16:46:11 +0300

    Minor change to button opacity

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -20,7 +20,7 @@ const Button: React.FC<ButtonProps> = ({
     icon: Icon
 }) => {
     return (
-        <button onClick={onClick} disabled={disabled} className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full 
+        <button onClick={onClick} disabled={disabled} className={`relative disabled:opacity-30 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full 
         ${outline ? 'bg-white' : 'bg-[#41644A]'} 
         ${outline ? 'border-black' : 'border-[#41644A]'} 
         ${outline ? 'text-black' : 'text-white'} 
